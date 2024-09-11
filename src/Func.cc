703e3d7335eb332faa0c98e1ad3530f23b1117d0
Vern Paxson <vern@corelight.com>
2024-09-11 16:50:32 +0200

    fixed lambda hash collision bug due to function descriptions lacking full parameter information

--- src/Func.cc
+++ src/Func.cc
@@ -672,6 +672,8 @@ std::optional<BrokerData> ScriptFunc::SerializeCaptures() const {
 
 void ScriptFunc::Describe(ODesc* d) const {
     d->Add(Name());
+    d->AddSP(":");
+    type->Describe(d);
 
     d->NL();
     d->AddCount(frame_size);
